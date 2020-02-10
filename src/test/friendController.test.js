72ae1b771585a8549498735aa3f86c177a086889
Mark-Ball <markball111@gmail.com>
2020-02-11 10:30:14 +1100

    finalised tests for create function

--- src/test/friendController.test.js
+++ src/test/friendController.test.js
@@ -1,13 +1,17 @@
 import { create } from './../../backend/controllers/friendController';
 import FriendModel from './../../backend/database/models/friendModel';
+import dbConnect from './../../backend/database/connect';
 
-beforeAll() {
+let mongoose;
 
-}
+beforeAll(async () => {
+    mongoose = await dbConnect(process.env.DB_HOST_TESTING);
+});
 
-afterAll() {
-    
-}
+afterAll(async () => {
+    await FriendModel.deleteMany({});
+    await mongoose.connection.close();
+});
 
 it('Tests can be run in friendController.test.js', () => {
     expect(true).toBe(true);
@@ -25,12 +29,11 @@ describe('Create functionality', () => {
     });
 
     describe('Integration tests', () => {
-        it('Entry created in db can be retrieved', () => {
+        it('Entry created in db can be retrieved', async () => {
             create({ name: 'Tom', age: 25 });
-            const user = FriendModel.find().sort({$natural:-1}).limit(1);
-            console.log(user);
-            expect(user.name).toEqual('Tom');
-            expect(user.age).toEqual(25);
+            const user = await FriendModel.find();
+            expect(user[user.length - 1].name).toEqual('Tom');
+            expect(user[user.length - 1].age).toEqual(25);
         });
     });
 });
